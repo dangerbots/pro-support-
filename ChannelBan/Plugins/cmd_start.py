fa3da330e4ba9dd6dad314a2efcdb15448fd4d7f
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2021-12-10 13:34:38 +0530

    Create cmd_start.py

---
+++ ChannelBan/Plugins/cmd_start.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Client.on_message(filters.command(["start", "start@ChannelBanRobot"]))
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
+ `Heya I'm A Anti Channel Tegram bot to delete and ban message sent by channel`""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Support", url="https://t.me/danger_bots"
+                    ),
+                    InlineKeyboardButton(
+                        "Devoleper", url="https://t.me/danger_of_telegram"
+                    ),
+                    "Owner✨️", url="https://t.me/Qalbeyy"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "ADD ME ❤", url="https://t.me/Banhammer_robot?startgroup=true"
+                    )
+                ]
+            ]
+        )
+    )
